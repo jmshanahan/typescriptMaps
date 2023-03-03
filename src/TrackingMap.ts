e71004c28a8c9b12fd0806d3be26e725013eba43
jmshanahan <comeraghsolutions@gmail.com>
2023-03-03 20:54:01 +1100

    Added HTMLElement

--- src/TrackingMap.ts
+++ src/TrackingMap.ts
@@ -12,10 +12,13 @@ export interface Entity {
 export class TrackingMap {
   private googleMap: google.maps.Map;
   constructor(elementId: string) {
-    this.googleMap = new google.maps.Map(document.getElementById(elementId), {
-      zoom: 1.6,
-      center: { lat: 0, lng: 20 },
-    });
+    this.googleMap = new google.maps.Map(
+      document.getElementById(elementId) as HTMLElement,
+      {
+        zoom: 10,
+        center: { lat: 0, lng: 20 },
+      }
+    );
   }
 
   attachMarker(entity: Entity): void {
